84ba22e6a4904eb4796457d937f8caa461dd991a
haanhtun1410 <vunblph27074@fpt.edu.vn>
2023-10-03 21:32:20 +0700

    ok

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
 
-  public static BASE_URL_API = 'http://localhost:8085'; // Địa chỉ cơ sở của API
+  public static BASE_URL_API = 'http://localhost:8088'; // Địa chỉ cơ sở của API
 
 
 }
