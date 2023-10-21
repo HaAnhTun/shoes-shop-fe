592f59375766eaf07f2409cc33710a1a75361d9b
hungnd2 <nguyenduyhung2003xx@gmail.com>
2023-10-21 20:32:45 +0700

    fix conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,45 +8,45 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AnimateModule } from "primeng/animate";
 import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DataViewModule } from 'primeng/dataview';
-import { DataViewLayoutOptions } from 'primeng/dataview';
-import { TagModule } from 'primeng/tag';
-import { ButtonModule } from 'primeng/button';
-import { RatingModule } from 'primeng/rating';
-import { HomeComponent } from './components/home/home.component';
-import { ShopComponent } from './components/shop/shop.component';
-import { ManageComponent } from './components/manage/manage.component';
-import { TableModule } from 'primeng/table';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DataViewModule } from "primeng/dataview";
+import { DataViewLayoutOptions } from "primeng/dataview";
+import { TagModule } from "primeng/tag";
+import { ButtonModule } from "primeng/button";
+import { RatingModule } from "primeng/rating";
+import { HomeComponent } from "./components/home/home.component";
+import { ShopComponent } from "./components/shop/shop.component";
+import { ManageComponent } from "./components/manage/manage.component";
+import { TableModule } from "primeng/table";
 import { DialogModule } from "primeng/dialog";
-import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component'; // Import the HttpClientModule
-import { Menubar, MenubarModule } from 'primeng/menubar';
-import { CarouselModule } from 'primeng/carousel';
-import { ImageModule } from 'primeng/image';
-import { GalleriaModule } from 'primeng/galleria';
-import { ChartModule } from 'primeng/chart';
-import { LoginComponent } from './components/login/login.component';
-import { FormGroup, FormsModule } from '@angular/forms';
-import { TokenInterceptor } from './shared/auth/token.interceptor';
-import { RegisterComponent } from './components/register/register.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { AppLayoutModule } from './layout/app.layout.module';
-import { ShoesCategoryComponent } from './components/shoes-category/shoes-category.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ProductComponent } from './components/product/product.component';
-import { AutoCompleteModule } from 'primeng/autocomplete';
-import { MessagesModule } from 'primeng/messages';
-import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { HttpClientModule } from "@angular/common/http";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component"; // Import the HttpClientModule
+import { Menubar, MenubarModule } from "primeng/menubar";
+import { CarouselModule } from "primeng/carousel";
+import { ImageModule } from "primeng/image";
+import { GalleriaModule } from "primeng/galleria";
+import { ChartModule } from "primeng/chart";
+import { LoginComponent } from "./components/login/login.component";
+import { FormGroup, FormsModule } from "@angular/forms";
+import { TokenInterceptor } from "./shared/auth/token.interceptor";
+import { RegisterComponent } from "./components/register/register.component";
+import { ForgotPasswordComponent } from "./components/forgot-password/forgot-password.component";
+import { AppLayoutModule } from "./layout/app.layout.module";
+import { ShoesCategoryComponent } from "./components/shoes-category/shoes-category.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ProductComponent } from "./components/product/product.component";
+import { AutoCompleteModule } from "primeng/autocomplete";
+import { MessagesModule } from "primeng/messages";
+import { ConfirmPopupModule } from "primeng/confirmpopup";
 import { ConfirmationService, MessageService } from "primeng/api";
-import { FileUploadModule } from 'primeng/fileupload';
-import { ToolbarModule } from 'primeng/toolbar';
-import { InputTextModule } from 'primeng/inputtext';
-import { DropdownModule } from 'primeng/dropdown';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { ToastModule } from 'primeng/toast';
+import { FileUploadModule } from "primeng/fileupload";
+import { ToolbarModule } from "primeng/toolbar";
+import { InputTextModule } from "primeng/inputtext";
+import { DropdownModule } from "primeng/dropdown";
+import { ConfirmDialogModule } from "primeng/confirmdialog";
+import { ToastModule } from "primeng/toast";
 import { BrandComponent } from "./components/brand/brand.component";
 import { OderComponent } from "./components/oder/oder.component";
 @NgModule({
@@ -92,10 +92,15 @@ import { OderComponent } from "./components/oder/oder.component";
     ToolbarModule,
     InputTextModule,
     ConfirmDialogModule,
-    ToastModule
+    ToastModule,
   ],
-  providers: [DataViewLayoutOptions, TokenInterceptor, MessageService, ConfirmationService],
+  providers: [
+    DataViewLayoutOptions,
+    TokenInterceptor,
+    MessageService,
+    ConfirmationService,
+  ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
