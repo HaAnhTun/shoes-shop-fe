6c4521e82f2e59dc386610a1e25175fbf208c19f
phansythanhtung <109521606+phansythanhtung@users.noreply.github.com>
2023-11-11 12:29:21 +0700

    commit

--- src/app/components/client/pay/pay.component.ts
+++ src/app/components/client/pay/pay.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./pay.component.css']
 })
 export class PayComponent {
-  selectedShipping: string;
-  paymentMethod: string;
+  basePrice: number = 1790000; // Giá cơ bản của sản phẩm
+  shippingCost: number = 0; // Phí giao hàng ban đầu là 0
+  totalPrice: number = this.basePrice + this.shippingCost; // Tổng giá ban đầu
+
+  updateShippingCost(cost: number) {
+    this.shippingCost = cost;
+    this.totalPrice = this.basePrice + this.shippingCost; // Cập nhật tổng giá
+  }
 }
